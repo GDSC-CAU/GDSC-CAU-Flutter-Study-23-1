18ebdb613681cdd47d5d34231127943549b2517e
falconlee236 <falconlee236@cau.ac.kr>
2023-11-25 17:16:38 +0900

    first button grow up

--- sangyun_lee/lib/pages/secondpage.dart
+++ sangyun_lee/lib/pages/secondpage.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 import 'package:sangyun_lee/widgets/diamondbutton.dart';
 
@@ -9,35 +11,70 @@ class SecondPage extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          const SingleChildScrollView(
+          SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: SizedBox(
-              height: 100,
-              child: Row(
-                children: [
-                  DiamondButton(icon: Icon(Icons.monitor)),
-                  DiamondButton(icon: Icon(Icons.account_balance_wallet)),
-                  DiamondButton(icon: Icon(Icons.shield_moon)),
-                  DiamondButton(icon: Icon(Icons.comment)),
-                  DiamondButton(icon: Icon(Icons.backpack)),
-                ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: SizedBox(
+                height: 100,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        right: 50.0,
+                        left: 20.0,
+                        top: 8.0,
+                      ),
+                      child: Transform.scale(
+                        scale: 1.3,
+                        child: Transform.rotate(
+                          angle: -math.pi / 4,
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            color: Colors.blueAccent,
+                            child: Transform.rotate(
+                              angle: math.pi / 4,
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: const Icon(Icons.monitor),
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const DiamondButton(
+                        icon: Icon(Icons.account_balance_wallet)),
+                    const DiamondButton(icon: Icon(Icons.shield_moon)),
+                    const DiamondButton(icon: Icon(Icons.comment)),
+                    const DiamondButton(icon: Icon(Icons.backpack)),
+                  ],
+                ),
               ),
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Text(
-                "Modern",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Modern",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
-              Text(
-                "Good quality item",
-                style: TextStyle(
-                  color: Colors.grey.shade600,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  "Good quality item",
+                  style: TextStyle(
+                    color: Colors.grey.shade600,
+                  ),
                 ),
               )
             ],
