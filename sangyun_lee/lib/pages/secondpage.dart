a81dc27568e50167ff8ca7432aea407de1cf89e3
falconlee236 <falconlee236@cau.ac.kr>
2023-11-26 13:49:32 +0900

    update second page

--- sangyun_lee/lib/pages/secondpage.dart
+++ sangyun_lee/lib/pages/secondpage.dart
@@ -91,11 +91,22 @@ class SecondPage extends StatelessWidget {
                       Container(
                         height: 300,
                         width: 200,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                          borderRadius: BorderRadius.circular(10.0),
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 15.0,
+                              offset: Offset(0.0, 0.75),
+                            ),
+                          ],
                         ),
-                        child: Text("hello"),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.0),
+                          child: const Image(
+                            fit: BoxFit.fill,
+                            image: AssetImage("images/wave.jpeg"),
+                          ),
+                        ),
                       ),
                       Positioned(
                         top: 280,
@@ -103,13 +114,52 @@ class SecondPage extends StatelessWidget {
                           width: 150,
                           height: 100,
                           decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10.0),
+                            color: Colors.grey.shade200,
+                            borderRadius: BorderRadius.circular(15.0),
                           ),
-                          child: const Column(
-                            children: [
-                              Text("hello"),
-                            ],
+                          child: const Padding(
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "LandScape",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                Text(
+                                  "Matter made",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Text("384"),
+                                    Text("USD"),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 310,
+                        left: 125,
+                        child: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          child: const Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
                           ),
                         ),
                       ),
